47363a2fdf24c69310b2dcc136e102d0e46140ac
Jaxe <jaxe.dev@gmail.com>
2021-03-02 22:55:31 +0800

    v1.7
    - Now has its own config in mod settings
    - Added new option "Show when drafted" (default: ON)
    - Added new option "Show combat interactions" (default: OFF)

--- Source/Data/Lang.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace Bubbles.Data
-{
-    internal static class Lang
-    {
-        public static string Get(string key, params object[] args) => string.Format((Mod.Id + "." + key).Translate(), args);
-    }
-}
--- Source/Interface/Bubbler.cs
+++ Source/Interface/Bubbler.cs
@@ -16,21 +16,22 @@ namespace Bubbles.Interface
         private const float OptimalHeight = 1050f;
 
         public static bool Visibility { get; set; } = true;
-        public static bool CanShow => Theme.Activated && Visibility && !WorldRendererUtility.WorldRenderedNow;
+        public static bool CanShow => Mod.Settings.Activated && Visibility && !WorldRendererUtility.WorldRenderedNow;
 
         private static readonly Dictionary<Pawn, List<Bubble>> Bubbles = new Dictionary<Pawn, List<Bubble>>();
 
         public static void Add(LogEntry entry, bool isCombat)
         {
-            if (!CanShow || (isCombat && !Theme.DoCombat)) { return; }
+            if (!CanShow || !Mod.Settings.DoCombat && isCombat) { return; }
             if (!isCombat && !(entry is PlayLogEntry_Interaction)) { return; }
 
             var initiator = Traverse.Create(entry).Field<Pawn>("initiator").Value;
             var recipient = Traverse.Create(entry).Field<Pawn>("recipient").Value;
-            if ((initiator == null) || (initiator.Map != Find.CurrentMap)) { return; }
+            if (initiator == null || initiator.Map != Find.CurrentMap) { return; }
 
-            if ((!initiator.Faction?.IsPlayer ?? true) && !Theme.DoNonPlayer) { return; }
-            if ((recipient?.RaceProps?.Animal ?? false) && !Theme.DoAnimals) { return; }
+            if (!Mod.Settings.DoNonPlayer && (!initiator.Faction?.IsPlayer ?? true)) { return; }
+            if (!Mod.Settings.DoAnimals && (recipient?.RaceProps?.Animal ?? false)) { return; }
+            if (!Mod.Settings.DoDrafted && ((initiator.drafter?.Drafted ?? false) || (recipient?.drafter?.Drafted ?? false))) { return; }
 
             if (!Bubbles.ContainsKey(initiator)) { Bubbles[initiator] = new List<Bubble>(); }
 
@@ -49,7 +50,7 @@ namespace Bubbles.Interface
             var correction = UI.screenHeight / OptimalHeight;
             var optimal = (CameraPlus.Loaded ? CameraPlus.CameraPlusOptimalZoom : OptimalZoom) * correction;
             var minZoom = OptimalMinZoom * correction;
-            var range = Mathf.Max((optimal - minZoom) * Theme.ScaleStart.ToPercentageFloat(), 1f);
+            var range = Mathf.Max((optimal - minZoom) * Mod.Settings.ScaleStart.ToPercentageFloat(), 1f);
             return Mathf.Min((Find.CameraDriver.CellSizePixels - minZoom) / range, 1f);
         }
 
@@ -58,36 +59,36 @@ namespace Bubbles.Interface
             if (!CanShow) { return; }
 
             var scale = GetScale();
-            if (scale <= Theme.MinScale.ToPercentageFloat()) { return; }
+            if (scale <= Mod.Settings.ScaleMin.ToPercentageFloat()) { return; }
 
             var selected = Find.Selector.SingleSelectedObject as Pawn;
 
             foreach (var pawn in Bubbles.Keys.OrderBy(pawn => pawn.Position.y).Where(pawn => pawn != selected).ToArray()) { DrawBubble(pawn, false, scale); }
 
-            if ((selected != null) && Bubbles.ContainsKey(selected)) { DrawBubble(selected, true, scale); }
+            if (selected != null && Bubbles.ContainsKey(selected)) { DrawBubble(selected, true, scale); }
         }
 
         private static void DrawBubble(Pawn pawn, bool isSelected, float scale)
         {
-            if (!pawn.Spawned || (pawn.Map != Find.CurrentMap) || pawn.Map.fogGrid.IsFogged(pawn.Position)) { return; }
+            if (!pawn.Spawned || pawn.Map != Find.CurrentMap || pawn.Map.fogGrid.IsFogged(pawn.Position)) { return; }
 
             var pos = GenMapUI.LabelDrawPosFor(pawn, -0.6f);
 
-            var offset = Mathf.CeilToInt(Theme.StartOffset * scale);
+            var offset = Mathf.CeilToInt(Mod.Settings.OffsetStart * scale);
             var count = 0;
 
             foreach (var bubble in Bubbles[pawn].ToArray())
             {
-                if (count > Theme.MaxPerPawn) { return; }
+                if (count > Mod.Settings.PawnMax) { return; }
                 if (!bubble.Draw(pos + GetOffset(offset), isSelected, scale)) { Remove(pawn, bubble); }
-                offset += (Theme.GetOffsetDirection().IsHorizontal ? bubble.Width : bubble.Height) + Theme.Spacing;
+                offset += (Settings.GetOffsetDirection().IsHorizontal ? bubble.Width : bubble.Height) + Mod.Settings.Spacing;
                 count++;
             }
         }
 
         private static Vector2 GetOffset(int offset)
         {
-            var direction = Theme.GetOffsetDirection().AsVector2;
+            var direction = Settings.GetOffsetDirection().AsVector2;
             return new Vector2(offset * direction.x, offset * direction.y);
         }
 
